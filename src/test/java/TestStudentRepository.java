00881d1a5fffba61eae3bbd3bd863adeb63a3c51
TsioryRamiadamahefa <153943203+TsioryR98@users.noreply.github.com>
2025-02-11 17:55:33 +0000

    feat(test) : add new test for TestStudentRepository

--- src/test/java/TestStudentRepository.java
+++ src/test/java/TestStudentRepository.java
@@ -5,7 +5,9 @@ import org.prog3.model.Student;
 import org.prog3.repository.Criteria;
 import org.prog3.repository.StudentRepository;
 
+import java.security.AuthProvider;
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,5 +46,39 @@ public class TestStudentRepository {
         criteriaListBoth.add(new Criteria("date_of_birth", birthRange));
         List<Student> studentListBothCriteria = subject.readStudentByCriteria(criteriaListBoth);
         assertEquals(11, studentListBothCriteria.size());
+    }
+
+    @Test 
+    void saveOrUpdate_pass() {
+        
+    }
+
+    @Test
+    void findById_pass(){
+        //create from the test
+
+        Student expectedStudent = studentTest();
+        Student acStudent = subject
+        .findById(expectedStudent.getStudentId());
+        assertEquals(expectedStudent,acStudent);
+
+        /*from database equals by lastname first name groupId */
+
+
+    }
+
+    private Student studentTest(){
+        Student expectedStudent = new Student();
+        int group_id = 3;
+        Group group = new Group();
+        group.setGroupId(group_id);
+        expectedStudent.setStudentId(100); 
+        expectedStudent.setLastName("student");
+        expectedStudent.setFirstName("test");
+        expectedStudent.setDateOfBirth(LocalDate.of(2000,1,1));
+        expectedStudent.setSex(sex.MALE);
+        expectedStudent.setGroup(group);
+
+        return expectedStudent;
     }
 }
